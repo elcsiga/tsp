2a1ac9f2cc390eae46d2065768aae6966c6c0769
Andras Kovacs <elcsiga@gmail.com>
2020-01-29 15:26:49 +0100

    routes

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PartnersComponent } from './pages/partners/partners.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ProjectsModule } from './pages/projects/projects.module';
+import { PartnersModule } from './pages/partners/partners.module';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: '', pathMatch: 'full', redirectTo: '/projects' },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'partners', component: PartnersComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    ProjectsModule,
+    PartnersModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
